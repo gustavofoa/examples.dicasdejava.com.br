4d4c1f37d08da86ad34a6351adcb60085fda2d09
Gustavo Furtado <gustavo.alves@mult-e.com.br>
2017-06-23 11:16:55 -0300

    Java sample code for create a List form an Array.

---
+++ src/br/com/dicasdejava/ArrayParaList.java
@@ -0,0 +1,15 @@
+package br.com.dicasdejava;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayParaList {
+
+    public static void main(String[] args) {
+	    String[] nomes = new String[]{"Gustavo", "Maria", "José", "João", "Ana"};
+
+		List listaDeNomes = Arrays.asList(nomes);
+
+		System.out.println(listaDeNomes);
+    }
+}
