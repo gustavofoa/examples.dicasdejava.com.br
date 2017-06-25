bd401fca9d79de142cddba9ce1974b5ad2ecbc55
Gustavo Furtado de Oliveira Alves <gustavo.foa@gmail.com>
2017-06-25 16:02:36 -0300

    New sample code: OperacoresBitwise

---
+++ src/br/com/dicasdejava/OperacoesBitwise.java
@@ -0,0 +1,23 @@
+package br.com.dicasdejava;
+
+public class OperacoesBitwise {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 12;
+
+		System.out.println(a + " | " + b + " = " + (a | b));
+		System.out.println(a + " & " + b + " = " + (a & b));
+		System.out.println(a + " ^ " + b + " = " + (a ^ b));
+
+		a = 9;
+		b = 7;
+
+		System.out.println(a + " | " + b + " = " + (a | b));
+		System.out.println(a + " & " + b + " = " + (a & b));
+		System.out.println(a + " ^ " + b + " = " + (a ^  b));
+
+	}
+
+}
