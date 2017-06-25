45926ada83f932cef7e095d7bbca46fe8a039642
Gustavo Furtado de Oliveira Alves <gustavo.foa@gmail.com>
2017-06-25 16:45:55 -0300

    New sample code: OperadoresLogicosDuploOuSimples

---
+++ src/br/com/dicasdejava/OperadoresLogicosDuploOuSimples.java
@@ -0,0 +1,41 @@
+package br.com.dicasdejava;
+
+public class OperadoresLogicosDuploOuSimples {
+
+	public static void main(String[] args) {
+
+		System.out.println("false && true = " +
+				(primeiraCondicao(false) && segundaCondicao(true)) +
+				" - sem teste da segunda condição.");
+
+		System.out.println();
+
+		System.out.println("false & true = " +
+				(primeiraCondicao(false) & segundaCondicao(true)) +
+				" - com teste da segunda condição.");
+
+		System.out.println();
+
+		System.out.println("true || false = " +
+			(primeiraCondicao(true) || segundaCondicao(false)) +
+			" - sem teste da segunda condição.");
+
+		System.out.println();
+
+		System.out.println("true | false = " +
+				(primeiraCondicao(true) | segundaCondicao(false)) +
+				" - com teste da segunda condição.");
+
+	}
+
+	private static boolean primeiraCondicao(boolean retorno){
+		System.out.println("Executou o teste da primeira condição");
+		return retorno;
+	}
+
+	private static boolean segundaCondicao(boolean retorno){
+		System.out.println("Executou o teste da segunda condição");
+		return retorno;
+	}
+
+}
