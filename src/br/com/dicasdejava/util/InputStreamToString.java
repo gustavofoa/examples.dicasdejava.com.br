7e935832be5cf505ec9e3cf955470359837ef8df
Gustavo Furtado de Oliveira Alves <gustavo.foa@gmail.com>
2017-07-29 20:55:08 -0300

    New example: InputStreamToString

---
+++ src/br/com/dicasdejava/util/InputStreamToString.java
@@ -0,0 +1,21 @@
+package br.com.dicasdejava.util;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class InputStreamToString {
+
+	public static void main(String[] args) throws IOException {
+
+		InputStream inputStream = new ByteArrayInputStream("Gustavo Furtado de Oliveira Alves".getBytes());
+
+		String textoConvertido = IOUtils.toString(inputStream);
+
+		System.out.println(textoConvertido);
+
+	}
+
+}
