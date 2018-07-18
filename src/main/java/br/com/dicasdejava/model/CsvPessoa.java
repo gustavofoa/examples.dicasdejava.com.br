31ffcd5cf4488fd30b418fd9a93cd048c86cd405
Gustavo Furtado de Oliveira Alves <gustavo.foa@gmail.com>
2018-07-18 16:18:05 -0300

    new Example and refactory

---
+++ src/main/java/br/com/dicasdejava/model/CsvPessoa.java
@@ -0,0 +1,36 @@
+package br.com.dicasdejava.model;
+
+public class CsvPessoa {
+    private String nome;
+    private Integer idade;
+    private String email;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "CsvPessoa{nome='" + nome + "\', idade=" + idade + ", email='" + email + "\'}";
+    }
+}
---
+++ src/main/java/br/com/dicasdejava/util/LerLinhasComoObjetosOpenCsv.java
@@ -0,0 +1,30 @@
+package br.com.dicasdejava.util;
+
+import br.com.dicasdejava.model.CsvPessoa;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class LerLinhasComoObjetosOpenCsv {
+
+    public static void main(String[] args) throws IOException {
+
+        Reader reader = Files.newBufferedReader(Paths.get("pessoas.csv"));
+
+        CsvToBean<CsvPessoa> csvToBean = new CsvToBeanBuilder(reader)
+                .withType(CsvPessoa.class)
+                .build();
+
+        List<CsvPessoa> pessoas = csvToBean.parse();
+
+        for (CsvPessoa pessoa : pessoas)
+            System.out.println(pessoa);
+
+    }
+
+}
